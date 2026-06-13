0a10615d46b21c80925cd193299c76c5ae8b3228
Homie Hood <petr.salmin@mail.ru>
2026-06-13 10:21:03 +0300

    добавил изменения в блоки

--- js/include.js
+++ js/include.js
@@ -14,6 +14,20 @@ async function loadComponent(id, file) {
             nav.classList.toggle("active");
 
         });
+
+        let currentPage = window.location.pathname.split("/").pop();
+
+        if(currentPage === ""){
+            currentPage = "index.html";
+        }
+
+        document.querySelectorAll(".navbar a").forEach(link => {
+
+            if(link.getAttribute("href") === currentPage){
+                link.classList.add("active");
+            }
+
+        });
     }
 }
 
